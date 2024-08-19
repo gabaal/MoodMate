8bfc81a564ed25a7d6d1ab5dca9c1a7a7cb6da93
gerald <geraldbaalham@live.co.uk>
2024-08-19 15:06:51 +0100

    First commit

---
+++ components/Calendar.jsx
@@ -0,0 +1,35 @@
+const months = {
+  January: "Jan",
+  February: "Feb",
+  March: "Mar",
+  April: "Apr",
+  May: "May",
+  June: "Jun",
+  July: "Jul",
+  August: "Aug",
+  September: "Sept",
+  October: "Oct",
+  November: "Nov",
+  December: "Dec",
+};
+const monthsArr = Object.keys(months);
+const now = new Date();
+const dayList = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export default function Calendar() {
+  const year = 2024;
+  const month = "August";
+  const monthNow = new Date(year, Object.keys(months).indexOf(month), 1);
+  const firstDayOfMonth = monthNow.getDay();
+  const daysInMonth = new Date(year, Object.keys(months).indexOf(month) + 1, 0);
+
+  return <div></div>;
+}
